2c41377f020d025addba574acb7b03736499a531
belencita28 <belenalristi@gmail.com>
2022-06-08 20:20:48 -0300

    goup button

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
 
-    
 });
+function scrollFunction() {
+   
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      $('#top').css('display','block');
+    } else {
+      $('#top').css('display','none');
+    }
+  }
+  
+  
+  function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;//chrome
+  }
